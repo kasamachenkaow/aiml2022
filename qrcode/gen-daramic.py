457bf28a1d0ebbae65c1c8200a32a4941bfd05cf
Kasama Chenkaow <kasama.chenkaow@klarna.com>
2023-12-19 22:06:12 +0100

    a lot of qrs

--- qrcode/gen-daramic.py
+++ qrcode/gen-daramic.py
@@ -6,15 +6,19 @@ def generate_qr_codes(texts):
     for text in texts:
         # Generate the QR code
         qr = qrcode.QRCode(version=1, box_size=10, border=5)
-        qr.add_data(text)
+        qr.add_data(text["url"])
         qr.make(fit=True)
 
         # Create an image from the QR code
         img = qr.make_image(fill_color="black", back_color="white")
 
         # Save the image as a PNG file with filename same as input text
-        img.save("qr11.png")
+        filename = "daramic/" + text["name"] + ".png"
+        img.save(filename)
 
 
-input_texts = ["https://verify.ssg.secure-smart-guard.com?qrCode=QR00011"]
+input_texts = [{
+    "url": "https://verify-psg.secure-smart-guard.com?area=daramic_hq&qrCode=QR{:05d}".format(i),
+    "name": "QR{:05d}".format(i),
+} for i in range(1, 51)]
 generate_qr_codes(input_texts)
